3828f867ec9fbe055d52dd7f221d71f2b16671d2
Teemu Rämä <teemu.rama@saunalahti.fi>
2024-02-06 23:23:16 +0200

    Lisätty tiedosto sisältöineen

---
+++ Testailua.py
@@ -0,0 +1,22 @@
+# Katotaan sujuuko commit ja silleen
+
+import random
+import sys
+
+def laskut(n):
+
+    try:
+        n = int(n)
+    
+    except ValueError:
+        print("Anna luku")
+        
+    else:
+        for _ in range(n):
+            a = random.randint(1,10)
+            b = random.randint(1,10)
+            print(f"{a} + {b} = {a+b}")
+
+
+if __name__ == "__main__":
+    laskut(sys.argv[1])
